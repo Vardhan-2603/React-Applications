723479bc761fd5c3186e429bf5fca689e6c96777
Vardhan-2603 <pubg26272828@gmail.com>
2026-03-12 18:09:13 +0530

    React-Applications

---
+++ React-Handson2-TaskManagerApp/src/App.jsx
@@ -0,0 +1,9 @@
+import TaskManager from './components/TaskManager.jsx';
+function App(){
+  return (
+    <div className='text-center bg-gray-400 p-10 '>
+      <h1 className='text-5xl'> </h1>
+      <TaskManager/>
+    </div>
+  )
+}
